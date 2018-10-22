afbffb051fc106f75e9b74d6e7860f5b3a799a06
johnbomba <bomba.john@gmail.com>
2018-10-22 17:35:07 -0400

    block

---
+++ block/new_secret_key.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+import os
+
+
+def new_2factor_key():
+    # create a random string to upload to the blockchain associated with a wallet address
+
+
+if __name__ == '__main__':
+    new_2factor_key()
