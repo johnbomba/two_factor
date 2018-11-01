4063a82fdaa1c558fe113006496a496b1affa337
johnbomba <bomba.john@gmail.com>
2018-11-01 17:16:26 -0400

    encrypt fix

--- block_writer/block_writer.py
+++ block_writer/block_writer.py
@@ -4,6 +4,7 @@ import os
 import sys
 import subprocess
 
+import binascii
 from flask import Flask
 
 app = Flask(__name__)
@@ -15,7 +16,7 @@ def new_key_to_block():
     tfa_address = '1Sr8mr4pXhcdPTDZhu74iyFAchM3Tt52ZZU5jE'
     login_address = '1FmqcFmFxQPbNJuKYvMSu61mWGV1iVtiTqxfUM'
 
-    # transaction id's for the login and 2fa public keys in the pubkeys stream 
+        # transaction id's for the login and 2fa public keys in the pubkeys stream 
     login_tx_id = 'c8935925db039376fb3778f5a97c14df493c2323f3486e86d02e8336e62af5da'
     tfa_tx_id = '8edb17e166c4ff26402f62dddb9282714de42de603827c2fece48898d2062d96'
 
@@ -47,11 +48,11 @@ def new_key_to_block():
 
 def generate_password():
     # use os.system to generate a pw that will encrypt the secret msg on the block chain 
-os.environ['PASSWORD'] = 'openssl rand -base64 48'
-password = subprocess.run('$PASSWORD', shell=True, stdout=subprocess.PIPE)
-password = password.stdout
-password = password.decode('utf-8')
-print(f'password {password}')
+    os.environ['PASSWORD'] = 'openssl rand -base64 48'
+    password = subprocess.run('$PASSWORD', shell=True, stdout=subprocess.PIPE)
+    password = password.stdout
+    password = password.decode('utf-8')
+    print(f'password {password}')
     print(type(password))
     return password
 
@@ -74,46 +75,54 @@ def secret_message():
 def publish_secret_message(block_address, cipher):
     # publish the secret message
     os.system(f"multichain-cli 2fact publishfrom {block_address} items '' {cipher}" )
+    print('message published')
+
 
 def publish_login_pw(block_address, login_label, encrypt_l_pw):
     # publish the encrypted login pw 
     os.system(f"multichain-cli 2fact publishfrom {block_address} access {login_label} {encrypt_l_pw}")
-
+    print('login published')
 
 def publish_tfa_pw(block_address,tfa_label, encrypt_t_pw):
     # publish the encrypted tfa pw
     os.system(f"multichain-cli 2fact publishfrom {block_address} access {tfa_label} {encrypt_t_pw}")
+    print('t published')
 
-
+# TODO rewrite this tomorrow needs subvproccess
 def encrypt_secret_message(secret_message, password):
     # used to encrypt the secret message on the block chain
     os.system(f"CIPHER=$(echo {secret_message} | openssl enc -aes-256-cbc -pass pass:{password} | xxd -p -c 99999)")
     cipher = os.environb.get('CIPHER')
+
     return cipher 
 
 def encrypt_login_pw(password):
-    # need to be able pass this to another function
-    # os.environ['ENCRYPT_L_PW'] = f"echo {password} | openssl rsautl -encrypt -inkey /tmp/l_pubkey.pem -pubin | xxd -p -c 9999"
-    
-    echo = subprocess.Popen(f'echo {password}', stdout=subprocess.PIPE)
-    ssl = subprocess.Popen(["openssl", "rsautl", "-encrypt", "-inkey", "/tmp/l_pubkey.pem", "-pubin"],stdin=echo.stdout, stdout=subprocess.PIPE)
-    hexer = subprocess.Popen(["xxd", "-p", "-c", "9999"],stdin=ssl.stdout, stdout=subprocess.PIPE)
 
+    openssl_echo = f'echo {password}'
+    openssl_echo2 = subprocess.Popen(openssl_echo, shell=True, stdout=subprocess.PIPE)
+    openssl = subprocess.Popen("openssl rsautl -encrypt -inkey /tmp/l_pubkey.pem -pubin -hexdump", shell=True, stdin=openssl_echo2.stdout, stdout=subprocess.PIPE)
 
-    encrypt_l_pw = subprocess.run('$ENCRYPT_L_PW', shell=True, stdout=subprocess.PIPE)
-    encrypt_l_pw = encrypt_l_pw.stdout
-    # os.system(f'ENCRYPT_L_PW=$(echo {password} | openssl rsautl -encrypt -inkey /tmp/l_pubkey.pem -pubin | xxd -p -c 9999)')
-    # encrypt_l_pw = os.environb.get('ENCRYPT_L_PW')
+    #encrypt_l_pw = subprocess.Popen(["xxd", "-p", "-c", "9999"],stdin=ssl.stdou$
+    encrypt_l_pw = openssl.stdout
+    encrypt_l_pw = encrypt_l_pw.read()
+
     print('login_pw created')
     return encrypt_l_pw
 
 def encrypt_tfa_pw(password):
     # need to be able pass this to another function
-    os.system(f'ENCRYPT_T_PW=$(echo {password} | openssl rsautl -encrypt -inkey /tmp/t_pubkey.pem -pubin | xxd -p -c 9999)')
-    encrypt_t_pw = os.environ.get('ENCRYPT_T_PW')
+    openssl_echo = f'echo {password}'
+    openssl_echo2 = subprocess.Popen(openssl_echo, shell=True, stdout=subprocess.PIPE)
+    openssl = subprocess.Popen("openssl rsautl -encrypt -inkey /tmp/t_pubkey.pem -pubin -hexdump", shell=True, stdin=openssl_echo2.stdout, stdout=subprocess.PIPE)
+
+    #encrypt_t_pw = subprocess.Popen(["xxd", "-p", "-c", "9999"],stdin=ssl.stdou$
+    encrypt_t_pw = openssl.stdout
+    encrypt_t_pw = encrypt_t_pw.read()
+
     print('tfa_pw created')
     return encrypt_t_pw
 
 if __name__ == '__main__':
     # app.run(debug=True)
     app.run(host= '0.0.0.0', debug=True)
+    
